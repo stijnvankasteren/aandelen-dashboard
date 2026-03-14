75749e71a7653dfdff877a42cfb3ebc38a4d0e32
stijnvankasteren <stijn@vankasteren.net>
2026-03-14 19:42:00 +0100

    update

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Kopieer standaard inputbestanden naar data volume (alleen als ze nog niet bestaan)
+cp -n /app/defaults/universe.json /app/data/universe.json
+cp -n /app/defaults/portfolio.json /app/data/portfolio.json
+
 # Start cron daemon op de achtergrond
 cron
 
