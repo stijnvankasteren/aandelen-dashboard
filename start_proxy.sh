eac2e88e02c2d0a860c985f344194e073b52c036
stijnvankasteren <stijn@vankasteren.net>
2026-03-14 19:18:52 +0100

    Update start_proxy.sh

--- start_proxy.sh
+++ start_proxy.sh
@@ -2,5 +2,8 @@
 # Start cron daemon op de achtergrond
 cron
 
+# Haal data op bij opstarten
+python3 fetch_data.py
+
 # Start de proxy op de voorgrond (zodat Docker de container draaiende houdt)
 exec python3 fetch_t212_proxy.py
